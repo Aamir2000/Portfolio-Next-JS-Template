9e61902f3965eb21c56b0bde434e2d64621dfb09
Aamir-MCSAM <aamir@webinfomart.com>
2023-12-19 18:11:04 +0530

    Fixed issue with images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        loader: "custom",
+        loaderFile: "./src/img-loader.ts",
         domains: [
             "images.pexels.com"
         ]
